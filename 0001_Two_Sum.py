91d90d0262cd546171418ef554a0ad0edf979724
ShihSyun Huang <shihsyun@gmail.com>
2019-05-05 11:14:03 +0800

    brute force approach

--- 0001_Two_Sum.py
+++ 0001_Two_Sum.py
@@ -12,16 +12,8 @@ Given nums = [2, 7, 11, 15], target = 9,
 Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
 
+Result: https://leetcode.com/submissions/detail/226689385/
 """
-
-
-# class Solution:
-#     def twoSum(self, nums: List[int], target: int) -> List[int]:
-
-#         for idx in range(len(nums)):
-#             for ptr in range(idx+1, len(nums)):
-#                 if nums[ptr] == target - nums[idx]:
-#                     return idx, ptr
 
 
 class Solution(object):
